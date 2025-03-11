30266586f001fefef5892e3fd944642e174736dd
masato-tsuji-wsl <gentarou7735@gmail.com>
2025-03-11 14:31:18 +0900

    first commit

---
+++ scripts/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# mainブランチに切り替え
+git checkout main
+
+# mainブランチの最新の変更をプッシュ
+git add .
+git commit -m "Update main branch with latest changes"
+git push origin main
+
+# gh-pagesブランチに切り替え
+git checkout gh-pages
+
+# mainブランチの変更をマージ
+git merge main
+
+# gh-pagesブランチの変更をプッシュ
+git push origin gh-pages
+
+# mainブランチに戻る
+git checkout main
---
+++ src/lib/notes.js
@@ -0,0 +1,4 @@
+'use strict'
+// 音符の情報をfirebaseから取得
+
+
