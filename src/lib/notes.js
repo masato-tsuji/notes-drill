6e53e58723eb7156569747247ee1ebd9eb35a825
masato-tsuji-wsl <masato_tsuji@mail.toyota.co.jp>
2025-03-12 12:22:28 +0900

    edit

--- src/lib/notes.js
+++ src/lib/notes.js
@@ -2,7 +2,45 @@
 // 音符の情報をfirebaseから取得し出題するスクリプト
 
 
+const objPiano = () => {
 
+  const initialize = () => {
+    //オプションエリア描画
+    function createCheckbox(id, label) {
+      return `
+          <div class="cnf_tgl">
+              <input type="checkbox" id="${id}">
+              <label for="${id}"></label>
+              <span class="class_name">${label}</span>
+          </div>
+      `;
+    }
+    const keyElement = document.createElement('div');
+
+  }
+
+  //オプション状態取得
+
+
+  //firebaseから誤答履歴取得
+
+
+  //firebaseから問題オブジェクト生成
+
+
+  //出題
+
+
+  //回答チェック
+
+
+  //結果と正解表示
+
+  //結果をfirebaseに登録
+
+
+  return initialize;
+}
 
 
 
