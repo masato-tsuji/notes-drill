de760aad0a2fc3b8e61092fc7903effb27ffc35b
masato-tsuji-wsl <gentarou7735@gmail.com>
2025-03-13 01:23:16 +0900

    edit

--- src/lib/notes.js
+++ src/lib/notes.js
@@ -75,7 +75,15 @@ const objNotes = () => {
   const divSvg = document.getElementById("note-area");
 
   let currentNoteIndex = 0;
-  const notes = ["C4", "D4", "E4", "F#4", "G4"]; // 音階リスト
+  const notes = 
+      ["C4", "D4", "E4", "F#4", "G4"]; // 音階リスト
+
+  const notesBass = [
+    'c/2', 'd/2', 'e/2', 'f/2', 'g/2', 'a/2', 'b/2', 'c/3', 'd/3', 'e/3', 'f/3', 'g/3', 'a/3', 'b/3',
+    'c/4', 'd/4', 'e/4', '#c/2', '#d/2', '#f/2', '#g/2', '#a/2', '#c/3', '#d/3', '#f/3', '#g/3', '#a/3',
+    '#c/4', '#d/4', 'bd/2', 'be/2', 'bg/2', 'ba/2', 'bb/2', 'bd/3', 'be/3', 'bg/3', 'ba/3', 'bb/3',
+    'bd/4', 'be/4'
+  ]
 
   function drawSvgNote() {
       // 既存のSVGをクリア
@@ -83,27 +91,33 @@ const objNotes = () => {
 
       // SVG用のRendererを作成
       const rendererSvg = new Renderer(divSvg, Renderer.Backends.SVG);
-      rendererSvg.resize(800, 320); // キャンバスサイズ
+      rendererSvg.resize(800, 400); // キャンバスサイズ
       const contextSvg = rendererSvg.getContext();
 
       contextSvg.scale(3.5, 3.5);   // 楽譜の大きさの倍率
 
       // 五線譜を描画
       // const staveSvg = new Stave(10, 40, 200);
-      const staveSvg = new Stave(60, -15, 110); // キャンバスの中の位置(Left, top, long)
-      // staveSvg.addClef("bass");
-      staveSvg.addClef("treble");
+      const staveSvg = new Stave(60, 5, 110); // キャンバスの中の位置(Left, top, long)
+      const clef = "bass";
+      staveSvg.addClef(clef);
+      // staveSvg.addClef("treble");
       staveSvg.setContext(contextSvg).draw();
 
       // 新しい音符を作成
-      // const noteSvg = new StaveNote({ keys: [notes[currentNoteIndex]], duration: "w" });
-      const noteSvg = new StaveNote({ keys: ["g/4"], duration: "w" });
-      noteSvg.addAccidental(0, new Accidental("#"));
+      const noteChoice = rndChoice(notesBass);
+      const note = noteChoice.substr(-3);
+      const noteSvg = new StaveNote({ clef: clef, keys: [note], duration: "w" });
+      // const noteSvg = new StaveNote({ clef: "bass", keys: ["e/4"], duration: "w" });
+      // noteSvg.addAccidental(0, new Accidental("#"));
       // noteSvg.addAccidental(0, new Accidental("b"));
 
       // シャープ追加（必要なら）
-      if (notes[currentNoteIndex].includes("#")) {
+      if (note.includes("#")) {
           noteSvg.addAccidental(0, new Accidental("#"));
+      }
+      if (note.includes("b")) {
+          noteSvg.addAccidental(0, new Accidental("b"));
       }
 
       // 音符を描画
